ea6140eeee580639e11c5ffb70971f9e9d152dda
ELLIOTTCABLE <git@ell.io>
2019-04-14 14:29:02 +0200

    (!! new NF) Require OCaml-version-specific ppx-sedlex.
    Still have yet to get everything wired together to *build* multiple ppx-sedlexes for each OCaml version, though. Hrm. That's gonna need some massive changes to the Travis config, and probably a new, heavy, CI-script.

--- scripts/package.js
+++ scripts/package.js
@@ -1,16 +1,15 @@
 // This is run on Travis, and presumably eventually AppVeyor, builds. It's supposed to be as
 // cross-platform as possible, unlike the adjacent release-script, which I only care if *I* can run.
-const pkg = require("../package.json"),
-   arch = require("arch"),
+const current_ppx_sedlex_id = require("ppx-sedlex/identify"),
    path = require("path"),
    fs = require("fs"),
    cpy = require("cpy"),
    makeDir = require("make-dir"),
    archiver = require("archiver")
 
-const zipfile = `ppx-sedlex-v${pkg.version}-${process.platform}-${arch()}.zip`,
+const zipfile = `ppx-sedlex-${current_ppx_sedlex_id}.zip`,
    dist_dir = "dist/",
-   submodule_ppx_dir = `ppx-sedlex/ppx-sedlex-v${pkg.version}-${process.platform}-${arch()}/`
+   submodule_ppx_dir = `ppx-sedlex/ppx-sedlex-${current_ppx_sedlex_id}/`,
    build_dir = "_build/install/default/lib/sedlex/ppx/",
    zip_dir = "ppx/",
    exe = "ppx.exe"
@@ -18,21 +17,21 @@ const zipfile = `ppx-sedlex-v${pkg.version}-${process.platform}-${arch()}.zip`,
 // FIXME: Does any of these even work on Windows
 ;(async () => {
    // Copy the executable to the submodule,
-   console.log(path.join(build_dir, exe) + " -> " + submodule_ppx_dir)
+   console.log(`Copy: ${path.join(build_dir, exe)} -> ${submodule_ppx_dir}`)
    await cpy(path.join(build_dir, exe), submodule_ppx_dir)
 
    // ... and again to the zip-directory,
-   console.log(path.join(build_dir, exe) + " -> " + zip_dir)
+   console.log(`Copy: ${path.join(build_dir, exe)} -> ${zip_dir}`)
    await cpy(path.join(build_dir, exe), zip_dir)
 
    // Create a zip-archive,
    const dist = await makeDir(dist_dir)
-   console.log(path.join(zip_dir, exe) + " >>> " + path.join(dist, zipfile))
+   console.log(`Zip: ${path.join(zip_dir, exe)} >>> ${path.join(dist, zipfile)}`)
    const output = fs.createWriteStream(path.join(dist, zipfile)),
       archive = archiver("zip", { zlib: { level: 9 } })
 
    output.on("close", function() {
-      console.log(">> Zipped: " + archive.pointer() + " total bytes")
+      console.log(`>> Zipped: ${archive.pointer()} total bytes`)
    })
 
    archive.pipe(output)
